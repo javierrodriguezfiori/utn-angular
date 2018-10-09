e360af637f848b18ab3d06af23bd024f4717b634
martin <jmcataldo@gmail.com>
2018-10-08 21:45:22 -0300

    item

--- proyecto-modulos/src/app/app.module.ts
+++ proyecto-modulos/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 import { ItemsModule } from './items/items.module';
 import { HomeModule } from './home/home.module';
 
-import { Routes, RouterModule } from '@angular/router';
-
 import { ItemsComponent } from './items/items/items.component';
+import { ItemComponent } from './items/item/item.component';
 import { HomeComponent } from './home/home/home.component'; 
 
 import { AppComponent } from './app.component';
 
 const ROUTES: Routes = [
   { path: "items", component: ItemsComponent },
+  { path: "items/:id", component: ItemComponent },
   { path: "home", component: HomeComponent },
-  { path: "**", redirectTo: "home", pathMatch: "full"},
+  { path: "", redirectTo: "home", pathMatch: "full"},
+  { path: "**", redirectTo: "home", pathMatch: "full"}
 ];
 
 @NgModule({
@@ -28,7 +32,9 @@ const ROUTES: Routes = [
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-
+    { provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }   
   ],
   bootstrap: [AppComponent]
 })
--- proyecto-modulos/src/app/items/items.module.ts
+++ proyecto-modulos/src/app/items/items.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemsComponent } from './items/items.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ItemsComponent],
+  declarations: [ItemsComponent, ItemComponent],
 })
 export class ItemsModule { }
