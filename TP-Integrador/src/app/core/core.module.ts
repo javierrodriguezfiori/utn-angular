362c3cd9491314f8626df122c3aab2d701bf989e
Javier Rodriguez <pires_mdp@hotmail.com>
2018-11-05 18:58:04 -0300

    pavadas de css

--- TP-Integrador/src/app/core/core.module.ts
+++ TP-Integrador/src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { RoutingModule } from 'src/app/routing/routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RoutingModule
   ],
-  declarations: [ HomeComponent, MenuComponent],
-  exports: [ MenuComponent ]
+  declarations: [ HomeComponent, MenuComponent, HeaderComponent, FooterComponent],
+  exports: [ MenuComponent, HeaderComponent, FooterComponent ]
 })
 export class CoreModule { }
